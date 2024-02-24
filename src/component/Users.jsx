23d5016a7e08a813577876585de0a625d8360256
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-24 02:21:53 +0100

    if there is no users button dispare and rest button apiers

--- src/component/Users.jsx
+++ src/component/Users.jsx
@@ -1,9 +1,14 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
 import { userData } from './hooks/UsersData.js'
 
 const Users = () => {
   const [users, setUsers] = useState(userData)
   const userTitle = useRef(null)
+
+  const resetUsers = useCallback(() => {
+    userTitle.current.textContent = 'Users'
+    setUsers(userData)
+  }, [userData])
 
   return (
     <>
@@ -20,14 +25,18 @@ const Users = () => {
             )
           })}
         </ul>
-        <button
-          onClick={() => {
-            setUsers([])
-            userTitle.current.textContent = 'users removed successfully'
-          }}
-        >
-          remove all
-        </button>
+        {users.length > 1 ? (
+          <button
+            onClick={() => {
+              setUsers([])
+              userTitle.current.textContent = 'users removed successfully'
+            }}
+          >
+            remove all
+          </button>
+        ) : (
+          <button onClick={resetUsers}>rest users</button>
+        )}
       </div>
     </>
   )
